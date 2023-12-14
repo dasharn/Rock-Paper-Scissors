ae721d56f87702cb26ed46df48dc8350b64ef535
Dasharn <dasharndennis@gmail.com>
2023-12-14 15:26:26 +0000

    Done

---
+++ tests/test_client.py
@@ -0,0 +1,49 @@
+import unittest
+from client import Client
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_draw_text(self):
+        # TODO: Implement test case
+        pass
+
+    def test_redraw_window(self):
+        # TODO: Implement test case
+        pass
+
+    def test_draw_waiting_for_player(self):
+        # TODO: Implement test case
+        pass
+
+    def test_draw_game_state(self):
+        # TODO: Implement test case
+        pass
+
+    def test_get_player_texts(self):
+        # TODO: Implement test case
+        pass
+
+    def test_handle_game_over(self):
+        # TODO: Implement test case
+        pass
+
+    def test_handle_mouse_event(self):
+        # TODO: Implement test case
+        pass
+
+    def test_update_game_state(self):
+        # TODO: Implement test case
+        pass
+
+    def test_main(self):
+        # TODO: Implement test case
+        pass
+
+    def test_menu_screen(self):
+        # TODO: Implement test case
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
